b8ae8ba346cc8ab912f4af0f63fef7ec28ebceb6
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-06-20 19:52:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const nextBtn = document.querySelector('.carousel__btn_next');
+const prevBtn = document.querySelector('.carousel__btn_prev');
+const list = document.querySelectorAll('.carousel__item');
+const dots = document.querySelectorAll('.carousel__dot');
+let card = 0;
+
+nextBtn.addEventListener('click', e => {
+  card++;
+  card = card >= 4 ? 0 : card;
+
+  for (const item of list) {
+    item.style.transform = `translateX(-${350 * (card)}px)`;
+  }
+
+  dots[card].classList.add('carousel__dot_active');
+
+  if (card === 0) {
+    dots[dots.length - 1].classList.remove('carousel__dot_active');
+  }
+  dots[card - 1].classList.remove('carousel__dot_active');
+});
+
+prevBtn.addEventListener('click', e => {
+  card--;
+  card = card < 0 ? 3 : card;
+
+  for (const item of list) {
+    item.style.transform = `translateX(-${350 * (card)}px)`;
+  }
+
+  dots[card].classList.add('carousel__dot_active');
+
+  if (card === 3) {
+    dots[0].classList.remove('carousel__dot_active');
+  }
+  dots[card + 1].classList.remove('carousel__dot_active');
+});
